0417860b3c8ad852c7b12eb553f3dedb98845e70
Andrius <andriusgel@gmail.com>
2018-08-15 13:16:24 +0300

    tobulinimas

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 $(document).ready(function(){
 
-if ($("#playBtn").click(function(){
-let value = $("#minutesInput").val();
-$(".bottom-selection").html(value);
+    let minutes = $("#minutesInput").val();
+    let timer2 = minutes;
+    $("#playBtn").click(function(){
 
-}));
+        let interval = setInterval(function(){
+            let timer = timer2.split(":");
+            let minutes = parseInt(timer[0], 10);
+            let seconds = parseInt(timer[1], 10);
+            --seconds;
+            minutes = (seconds < 0) ? --minutes : minutes;
+            seconds = (seconds < 0) ? 59 : seconds;
+            seconds = (seconds < 10) ? '0' + seconds : seconds;
+            $(".bottom-selection").html(minutes+ ":"+ seconds);
+            if (minutes < 0) clearInterval(interval);
+            if ((seconds <= 0) && (minutes <= 0)) clearInterval(interval);
+            timer2 = minutes + ':' + seconds;
+    
+        }, 1000);
+    })
+
 });
+
 
