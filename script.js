5ce892292d0bb2df37fa0a984f05a8f2f767ceb7
Andrius <andriusgel@gmail.com>
2018-08-15 21:24:48 +0300

    tobulinimas

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ $(document).ready(function(){
     
 
         //timer mechanism
-        
+        // if (running){
             let interval = setInterval(function(){  
-
+             
                 let timer = timer2.split(":");
                 let minutes = parseInt(timer[0], 10);
                 let seconds = parseInt(timer[1], 10);
@@ -20,23 +20,26 @@ $(document).ready(function(){
                 seconds = (seconds < 0) ? 59 : seconds;
                 seconds = (seconds < 10) ? '0' + seconds : seconds;
                 $(".bottom-selection").html(minutes+ ":"+ seconds); //put time value
-                if (running){
+
                     if ((seconds <= 0) && (minutes <= 0)) {
                         clearInterval(interval);
                         alert("Times Up");
                     } else{
                         timer2 = minutes + ':' + seconds;
                     }
-                }
+               
 
 
             }, 1000);
- 
+        // }else{
+        //     clearInterval(interval);
+        // }
 
 // if you press stop button it will stop timer
         $(document).on("click","#stopBtn", function() {
-            clearInterval(interval); 
             running = false;
+            clearInterval(interval); 
+
             console.log(timer2); 
             $("#resumeBtn").click(function(){
                 running = true;
