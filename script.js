91cfb1dc5b73be59c58dd218b39cb8a75c81b41c
Andrius <andriusgel@gmail.com>
2018-08-15 19:04:05 +0300

    tobulinimas
    Bandau sutvarkyti resume mygtuka

--- script.js
+++ script.js
@@ -1,33 +1,50 @@
 $(document).ready(function(){
+    let paused = false;
 
 //if you pressed start button, you will start timer
     $("#playBtn").click(function(){
         //read input value
         let minuteInput = $("#minutesInput").val(); 
         let timer2 = minuteInput + ":00";
+        let running = true;
+
         //timer mechanism
-        let interval = setInterval(function(){  
-            let timer = timer2.split(":");
-            let minutes = parseInt(timer[0], 10);
-            let seconds = parseInt(timer[1], 10);
-            --seconds;
-            minutes = (seconds < 0) ? --minutes : minutes;
-            seconds = (seconds < 0) ? 59 : seconds;
-            seconds = (seconds < 10) ? '0' + seconds : seconds;
-            $(".bottom-selection").html(minutes+ ":"+ seconds); //put time value
-            if ((seconds <= 0) && (minutes <= 0)){
-                clearInterval(interval);
-                alert("Times Up");
-            } else{
-                timer2 = minutes + ':' + seconds;
-            }
-        }, 1000);
+        
+            let interval = setInterval(function(){  
+
+                let timer = timer2.split(":");
+                let minutes = parseInt(timer[0], 10);
+                let seconds = parseInt(timer[1], 10);
+                --seconds;
+                minutes = (seconds < 0) ? --minutes : minutes;
+                seconds = (seconds < 0) ? 59 : seconds;
+                seconds = (seconds < 10) ? '0' + seconds : seconds;
+                $(".bottom-selection").html(minutes+ ":"+ seconds); //put time value
+                if (running){
+                    if ((seconds <= 0) && (minutes <= 0)) {
+                        clearInterval(interval);
+                        alert("Times Up");
+                    } else{
+                        timer2 = minutes + ':' + seconds;
+                    }
+                }
+
+
+            }, 1000);
+ 
+
 // if you press stop button it will stop timer
         $(document).on("click","#stopBtn", function() {
-            clearInterval(interval);   
+            clearInterval(interval); 
+            running = false
+            console.log(timer2); 
          });
+         $(document).on("click","#resumeBtn", function() {
+            running = true;
+         });
+        
+         
     });  
-
 });
 
 
