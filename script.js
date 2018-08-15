da4db97b5feeffdc50160257224fa6bdc1ddfcea
Andrius <andriusgel@gmail.com>
2018-08-15 16:30:46 +0300

    tobulinimas
    sutvarkytas stop button

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
 
-
+//if you pressed start button, you will start timer
     $("#playBtn").click(function(){
-        let minuteInput = $("#minutesInput").val();
+        //read input value
+        let minuteInput = $("#minutesInput").val(); 
         let timer2 = minuteInput + ":00";
-        let interval = setInterval(function(){
+        //timer mechanism
+        let interval = setInterval(function(){  
             let timer = timer2.split(":");
             let minutes = parseInt(timer[0], 10);
             let seconds = parseInt(timer[1], 10);
@@ -12,16 +14,18 @@ $(document).ready(function(){
             minutes = (seconds < 0) ? --minutes : minutes;
             seconds = (seconds < 0) ? 59 : seconds;
             seconds = (seconds < 10) ? '0' + seconds : seconds;
-            $(".bottom-selection").html(minutes+ ":"+ seconds);
+            $(".bottom-selection").html(minutes+ ":"+ seconds); //put time value
             if ((seconds <= 0) && (minutes <= 0)){
                 clearInterval(interval);
                 alert("Times Up");
             } else{
                 timer2 = minutes + ':' + seconds;
             }
-           
-    
         }, 1000);
+// if you press stop button it will stop timer
+        $(document).on("click","#stopBtn", function() {
+            clearInterval(interval);   
+         });
     });  
 
 });
